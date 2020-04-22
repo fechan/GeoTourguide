933b8aed7911eb8c4ba020a27953230cef1f4cb0
F. Chan <edu.fechan@gmail.com>
2020-04-21 20:57:03 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,57 @@
+"use strict";
+(function() {
+  const CORSANYWHERE = "https://cors-anywhere.herokuapp.com";
+  const OOCITIES = "https://www.oocities.org";
+
+  let hoodSelector = document.getElementById("neighborhood-select");
+  hoodSelector.addEventListener("change", changeNeighborhoods);
+  changeNeighborhoods();
+
+  /**
+   * Updates the directory of available sites in the nav
+   */
+  function changeNeighborhoods() {
+    let neighborhood = document.getElementById("neighborhood-select");
+    neighborhood = neighborhood.value;
+    
+    let xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+      let html = this.responseXML;
+      let links = extractLinks(html).filter(linksToSite);
+      let siteList = document.getElementById("site-list");
+      siteList.innerHTML = "";
+      for (let link of links) {
+        let site = document.createElement("a");
+        site.textContent = link.textContent;
+        site.href = `${OOCITIES}/${neighborhood}/${link.getAttribute("href")}`;
+        site.target = "archive-display";
+        let listItem = document.createElement("li");
+        listItem.appendChild(site);
+        siteList.append(listItem);
+      }
+    }
+    xhr.open("GET", `${CORSANYWHERE}/${OOCITIES}/${neighborhood}`);
+    xhr.responseType = "document";
+    xhr.send();
+  }
+
+  /**
+   * Extracts an array of <a> tags from Oocities directory for a neighborhood
+   * @param {HTMLDocument} html html document to search for links in
+   * @returns {Array} list of links
+   */
+  function extractLinks(html) {
+    //this will break if oocities changes, which is why it's its own function
+    return Array.from(html.querySelectorAll("a"));
+  }
+
+  /**
+   * Checks if <a> link directs to an actual archived site
+   * @param {HTMLElement} link the <a> to check
+   * @returns {Boolean} whether link is an archived site or not
+   */
+  function linksToSite(link) {
+    //this will break if oocities changes, which is why it's its own function
+    return link.href.includes(link.textContent);
+  }
+})();
